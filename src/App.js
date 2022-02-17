2df6f85f1eb36b542077599c387b55a9a5f6790a
henrietta_21 <haryormeedhey@gmail.com>
2022-02-17 12:45:06 +0100

    updated to a cleaner code

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
 import './App.css';
+import img from './assets/ProfilePicture.jpg'
 import React, { Component } from 'react'
 
 class App extends Component {
   state = {
-    person : {
-      fullName : ' ',
-      bio : " ",
-      profession : '',
-      image : ''
+    person :{
+      fullName :'Apata Henrietta',
+      bio :"An undergraduate with a spice",
+      profession : 'Student',
+      image : img  
     },
     show : true,
     seconds : 0
   }
 
-
-  handleOpenProfile = ()=> {
+  handleClick = () =>{
     this.setState({
-      show : false,
-      person :{
-        fullName :'Apata Henrietta',
-        bio :"An undergraduate with a spice",
-        profession : 'Student',
-        image : 'ProfilePicture.jpg'  
-      }
-    });
-  }
-
-  handleCloseProfile = () =>{
-    this.setState({
-      show : true,
-      person :{
-        fullName :'',
-        bio :" ",
-        profession : '',
-        image : ''  
-      }
+      show : !this.state.show
     });
   }
   
-  
-  time() {
-    this.setState(prevState => ({
-      seconds: prevState.seconds + 1
-    }));
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(()=>this.time(), 1000);
-  }
-
   
 
   
@@ -56,14 +27,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <h1>{this.state.person.fullName}</h1>
-          <h1>{this.state.person.bio}</h1>
-          <h1>{this.state.person.profession}</h1>
-          <img  style={{width:'250px'}}  src={this.state.person.image} alt="" />
-        </div>
-        <button onClick={this.state.show ? this.handleOpenProfile : this.handleCloseProfile}>{this.state.show ? 'Show': 'Hide'}</button>
-        <p>This component was mounted {this.state.seconds}s ago</p>
+        {
+          this.state.show ? (
+            <div>
+            <h1>{this.state.person.fullName}</h1>
+            <h1>{this.state.person.bio}</h1>
+            <h1>{this.state.person.profession}</h1>
+            <img  style={{width:'250px'}}  src={this.state.person.image} alt="" />
+          </div>
+          ):
+          ''
+        }
+        <button onClick={this.handleClick}>{this.state.show ? 'Show': 'Hide'}</button>
+        
       </div>
     );
   }
